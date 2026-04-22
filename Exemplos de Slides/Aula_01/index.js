ed4156b244484618b29cc1643d8f8bf5fb449186
Danilson Sanches <dsanches06@outlook.com>
2026-04-22 15:23:02 +0100

    feat: create my first gemini project

---
+++ Exemplos de Slides/Aula_01/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
+dotenv.config({ path: "../.env" });
+
+//agora, vamos criar uma instância do cliente do GenAI usando a chave de API que definimos no arquivo .env
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log("\n--- Answer ---");
+  console.log(response.text);
+}
+
+main();
